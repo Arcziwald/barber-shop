af11fd091bb459cb81e3d1eaa3231fa387246c9d
Artur Mochnia <amochnia@gmail.com>
2023-10-04 22:15:39 +0100

    added App

---
+++ src/core/App/App.js
@@ -0,0 +1,21 @@
+import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
+import { Evas } from "../../features/Evas";
+import { Barber } from "../../features/Barber";
+import { Shop } from "../../features/Shop";
+import { Header } from "../../common/Header";
+import { TilesList } from "../../common/TilesList";
+import { Box } from "../../common/Box";
+
+export const App = () => (
+  <HashRouter>
+    <Header />
+    <Box />
+    <TilesList />
+    <Switch>
+      <Route path="/evas" component={Evas} />
+      <Route path="/barber" component={Barber} />
+      <Route path="/shop" component={Shop} />
+      <Redirect to="/" />
+    </Switch>
+  </HashRouter>
+);
