1bbefd2d8bdc96c23c5fb4f2990fdb07c6de6643
Artur Mochnia <amochnia@gmail.com>
2023-10-04 22:15:28 +0100

    added theme

---
+++ src/core/theme.js
@@ -0,0 +1,33 @@
+export const theme = {
+    colors: {
+      black: "#000000",
+      mystic: "#e4e6f0",
+      woodSmoke: "#18181B",
+      whisper: "#F5F5FA",
+      waterloo: "#7E839A",
+      white: "#FFFFFF",
+      stormGray: "#74788B",
+      pattensBlue: "#D6E4FF",
+      darkerGrey: "#7E839A",
+      scienceBlue: "#0044CC",
+      denim: "#1169D4",
+      redArt: "#d50c00",
+      boxTiles: "rgb(228, 228, 226, 0.4)",
+    },
+  
+    breakpoints: {
+      desktop: "1300px",
+      tablet: "1200px",
+      tabletAlternative: "1050px",
+      hugeMobile: "850px",
+      defaultMobile: "767px",
+      mediumMobile: "550px",
+      smallMobile: "450px",
+      tinyMobile: "320px",
+    },
+  
+    common: {
+      boxShadow: "0px 4px 12px 0px #BAC7D580",
+    },
+  };
+  
