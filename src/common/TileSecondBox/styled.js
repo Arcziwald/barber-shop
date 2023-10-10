ca9555d63be3f14a77acd5f4c6c7091dd86a301b
Artur Mochnia <amochnia@gmail.com>
2023-10-10 22:41:15 +0100

    update responsive

--- src/common/TileSecondBox/styled.js
+++ src/common/TileSecondBox/styled.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  padding: 16px;
   margin-top: 10px;
   background: ${({ theme }) => theme.colors.white};
   background-color: rgba(255, 255, 255, 0.2);
@@ -9,17 +8,17 @@ export const Wrapper = styled.div`
   transition: transform 0.3s ease;
   transform-origin: center;
   border: none;
-  border-radius: 5px;
+  border-radius: 15px;
   display: flex;
   justify-content: space-around;
   flex-direction: column-reverse;
   align-items: center;
- 
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    padding: 8px;
+  }
+
   @media (max-width: ${({ theme }) => theme.breakpoints.smallMobile}) {
-    display: grid;
-    grid-template-columns: 130px auto;
-    grid-template-rows: auto 100%;
-    justify-content: unset;
   }
 
   &:hover {
@@ -27,36 +26,18 @@ export const Wrapper = styled.div`
   }
 `;
 
-
-
-export const Title = styled.span`
-  font-size: 40px;
-  font-weight: 600;
-  line-height: 130%;
-  color: ${({ theme }) => theme.colors.black};
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
-    font-size: 20px;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.defaultMobile}) {
-    font-size: 18px;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.smallMobile}) {
-    font-size: 16px;
-  }
+export const StyledTitle = styled.img`
+  width: 80%;
 `;
 
 export const StyledImgGallery = styled.img`
-  width: 100%; 
-  
+  border-radius: 15px;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.defaultMobile}) {
-    width: 40%; 
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    width: 100%;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.smallMobile}) {
-    width: 30%; 
+    width: 100%;
   }
 `;
