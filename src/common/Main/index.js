d9ecf34171e339b69926afbcdfe8843e10e14138
Artur Mochnia <amochnia@gmail.com>
2023-10-10 22:39:59 +0100

    added Main

--- src/common/Main/index.js
+++ src/common/Main/index.js
@@ -4,8 +4,8 @@ export const Main = styled.div`
   width: 1368px;
   max-width: calc(100% - 2 * 12px);
   margin: auto;
-  
-  @media (max-width: ${({ theme }) => theme.breakpoints.defaultMobile}) {
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
     margin: 16px auto;
   }
 `;
