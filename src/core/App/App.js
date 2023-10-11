4ad7887194b7778118be397abe79ab0a3dfd4b64
Artur Mochnia <amochnia@gmail.com>
2023-10-11 21:28:22 +0100

    update root

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -1,23 +1,19 @@
 import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
-import { Evas } from "../../features/Evas";
-import { Barber } from "../../features/Barber";
-import { Shop } from "../../features/Shop";
 import { Header } from "../../common/Header";
 import { TilesList } from "../../common/TilesList";
 import { Box } from "../../common/Box";
+import { Contact } from "../../common/TilesList/Contact";
+
 
 
 export const App = () => {
-  
   return (
     <HashRouter>
       <Header />
       <Box />
       <TilesList />
       <Switch>
-        <Route path="/evas" component={Evas} />
-        <Route path="/barber" component={Barber} />
-        <Route path="/shop" component={Shop} />
+        <Route exact path="/contact" component={Contact} />
         <Redirect to="/" />
       </Switch>
     </HashRouter>
