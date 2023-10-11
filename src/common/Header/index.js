d85b78e7565c508257391f9360e15b5d66596240
Artur Mochnia <amochnia@gmail.com>
2023-10-11 22:12:08 +0100

    added link to back main

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import { Main } from "../Main";
 import { Wrapper, Image } from "./styled";
 
 export const Header = () => (
   <Main>
     <Wrapper>
-    <Image />
-  </Wrapper>
+      <Link to="/evas">
+        <Image />
+      </Link>
+    </Wrapper>
   </Main>
 );
