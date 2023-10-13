e8adc91f90509c8fc6029ab1f8034702f2db3139
Artur Mochnia <amochnia@gmail.com>
2023-10-13 23:04:14 +0100

    added style to gallery

---
+++ src/js/Gallery/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  /* Twoje style CSS tutaj */
+  .pswp-gallery a {
+    margin: 10px;
+    display: flex;
+    width: 100%;
+  }
+`;
