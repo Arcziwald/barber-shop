2c45853bfca0cce413eeb00552535ad098609bcd
Artur Mochnia <amochnia@gmail.com>
2023-11-06 15:17:54 +0000

    added server

---
+++ node/bacup/barber_server/server.js
@@ -0,0 +1,11 @@
+
+const http = require("http")
+
+http.createServer(
+    function(req, res) {
+        console.log("Request");
+        res.writeHead(200, {"Content-type" : "text/html"});
+
+        res.end("Hello World!!");
+    }
+).listen(8080);
