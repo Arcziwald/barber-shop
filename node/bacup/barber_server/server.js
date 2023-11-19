76189986c4038bed4cbe1cde27cc2365391ee71d
Artur Mochnia <amochnia@gmail.com>
2023-11-19 13:44:03 +0000

    added server database

--- node/bacup/barber_server/server.js
+++ node/bacup/barber_server/server.js
@@ -1,11 +1,65 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const http = require("http")
+const app = express();
+const PORT = 3001; // Możesz użyć dowolnego wolnego portu
 
-http.createServer(
-    function(req, res) {
-        console.log("Request");
-        res.writeHead(200, {"Content-type" : "text/html"});
+app.use(cors());
+app.use(bodyParser.json());
 
-        res.end("Hello World!!");
-    }
-).listen(8080);
+// Połączenie z bazą danych
+mongoose.connect("mongodb://localhost:27017/haircutReservation", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Model danych
+const Reservation = mongoose.model("Reservation", {
+  name: String,
+  phoneNumber: String,
+  email: String,
+  selectedService: String,
+  selectedStylist: String,
+  date: String,
+  time: String,
+  additionalNotes: String,
+});
+
+// Obsługa żądania POST od formularza
+app.post("/submitReservation", async (req, res) => {
+  const {
+    name,
+    phoneNumber,
+    email,
+    selectedService,
+    selectedStylist,
+    date,
+    time,
+    additionalNotes,
+  } = req.body;
+
+  try {
+    const reservation = new Reservation({
+      name,
+      phoneNumber,
+      email,
+      selectedService,
+      selectedStylist,
+      date,
+      time,
+      additionalNotes,
+    });
+
+    await reservation.save();
+    res.status(200).json({ message: "Dane zapisane do bazy danych!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Błąd podczas zapisywania danych do bazy" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Serwer uruchomiony na porcie ${PORT}`);
+});
