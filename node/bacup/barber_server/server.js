89762ca999713365d17d3a034d1ff9c17a81d0b0
Artur Mochnia <amochnia@gmail.com>
2023-11-19 18:05:44 +0000

    activating the registration function

--- node/bacup/barber_server/server.js
+++ node/bacup/barber_server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const PORT = 3001; // Możesz użyć dowolnego wolnego portu
+const PORT = 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -25,7 +25,11 @@ const Reservation = mongoose.model("Reservation", {
   date: String,
   time: String,
   additionalNotes: String,
+  lockTime: { type: Date, default: null }, // Dodane pole lockTime
 });
+
+// Lista zarezerwowanych terminów
+let busyTimes = [];
 
 // Obsługa żądania POST od formularza
 app.post("/submitReservation", async (req, res) => {
@@ -34,29 +38,61 @@ app.post("/submitReservation", async (req, res) => {
     phoneNumber,
     email,
     selectedService,
-    selectedStylist,
+    selectedOderService,
     date,
     time,
     additionalNotes,
   } = req.body;
 
   try {
-    const reservation = new Reservation({
-      name,
-      phoneNumber,
-      email,
-      selectedService,
-      selectedStylist,
+    const existingReservation = await Reservation.findOne({
       date,
       time,
-      additionalNotes,
+      lockTime: { $ne: null }, // Sprawdzamy, czy termin jest zablokowany
     });
 
-    await reservation.save();
-    res.status(200).json({ message: "Dane zapisane do bazy danych!" });
+    if (existingReservation) {
+      res.status(409).json({ error: "Termin jest już zajęty. Wybierz inny termin." });
+    } else {
+      const reservation = new Reservation({
+        name,
+        phoneNumber,
+        email,
+        selectedService,
+        selectedOderService,
+        date,
+        time,
+        additionalNotes,
+        lockTime: new Date(), // Ustawiamy czas blokady
+      });
+
+      await reservation.save();
+      res.status(200).json({ message: "Dane zapisane do bazy danych!" });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Błąd podczas zapisywania danych do bazy" });
+  }
+});
+app.get("/", (req, res) => {
+  res.send("Serwer działa poprawnie");
+});
+// Pobierz zarezerwowane terminy
+app.get("/getReservedTimes", (req, res) => {
+  res.status(200).json({ reservedTimes: busyTimes });
+});
+
+app.get("/getLockedTimes", async (req, res) => {
+  try {
+    const lockedTimes = await Reservation.find({
+      lockTime: { $ne: null },
+    }).select("date time -_id");
+
+    const formattedLockedTimes = lockedTimes.map((lock) => `${lock.date} ${lock.time}`);
+    res.status(200).json({ lockedTimes: formattedLockedTimes });
+  } catch (error) {
+    console.error("Błąd pobierania zablokowanych terminów:", error);
+    res.status(500).json({ error: "Błąd podczas pobierania zablokowanych terminów" });
   }
 });
 
--- src/features/Booking/index.js
+++ src/features/Booking/index.js
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import { Main } from "../../common/Main";
 import { Title, Wrapper } from "./styled";
 
-// Zakładamy, że masz dostęp do listy zajętych terminów w formie tablicy busyTimes
-const busyTimes = ["2023-11-01 10:00", "2023-11-01 14:30", "2023-11-02 15:15"];
-
 class HaircutReservationForm extends Component {
   constructor(props) {
     super(props);
@@ -13,61 +10,147 @@ class HaircutReservationForm extends Component {
       phoneNumber: "",
       email: "",
       selectedService: "",
-      selectedStylist: "",
+      selectedOderService: "", // Poprawiona nazwa pola
       date: "",
       time: "",
       additionalNotes: "",
+      message: "", // Dodane pole message do stanu
+      busyTimes: [],
+      reservationDuration: 30,
     };
   }
 
-  // Definicja funkcji getMinDate
-  getMinDate = () => {
-    const today = new Date().toISOString().split('T')[0];
-    return today;
+  componentDidMount() {
+    this.getReservedTimes(); // Dodane wywołanie funkcji pobierającej zarezerwowane terminy
   }
+
+  // Dodana funkcja do pobierania zarezerwowanych terminów
+  getReservedTimes = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/getReservedTimes");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      this.setState({ busyTimes: data.reservedTimes });
+    } catch (error) {
+      console.error("Błąd pobierania zarezerwowanych terminów:", error);
+    }
+  };
+
+  // Dodana funkcja do pobierania minimalnej daty na podstawie zarezerwowanych terminów
+  getMinDate = () => {
+    const today = new Date().toISOString().split("T")[0];
+    return today;
+  };
 
   handleInputChange = (event) => {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
-  }
+    if (name === "selectedService") {
+      const reservationDuration = this.calculateReservationDuration(value);
+      this.setState({ [name]: value, reservationDuration });
+    } else {
+      this.setState({ [name]: value });
+    }
+  };
+
+  calculateReservationDuration = (selectedService) => {
+    switch (selectedService) {
+      case "Strzyżenie":
+        return 30;
+      case "Koloryzacja":
+        return 120;
+      case "Stylizacja":
+        return 60;
+      case "Solarium stojące":
+      case "Solarium leżące":
+        return 10;
+      default:
+        return 30;
+    }
+  };
 
   isTimeSlotAvailable = () => {
-    const selectedDateTime = `${this.state.date} ${this.state.time}`;
-    return !busyTimes.includes(selectedDateTime);
-  }
+    const { date, time, selectedService } = this.state;
+    const selectedDateTime = `${date} ${time}`;
+  
+    // Sprawdzamy, czy termin jest już zarezerwowany
+    if (this.state.busyTimes.includes(selectedDateTime)) {
+      return false;
+    }
+  
+    // Sprawdzamy, czy termin jest zablokowany dla danej usługi fryzjerskiej
+    const isTimeSlotBlocked = this.state.busyTimes.some((busyTime) => {
+      const [busyDateTime, busyService] = busyTime.split(" ");
+      return busyDateTime === selectedDateTime && busyService !== selectedService;
+    });
+  
+    return !isTimeSlotBlocked;
+  };
 
   handleSubmit = async (event) => {
     event.preventDefault();
+    const { date, time, reservationDuration } = this.state;
+
     if (this.isTimeSlotAvailable()) {
       try {
-        const response = await fetch("http://localhost:3001/submitReservation", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(this.state),
-        });
-  
+        const response = await fetch(
+          "http://localhost:3001/submitReservation",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              ...this.state,
+              endTime: this.calculateEndTime(time, reservationDuration),
+            }),
+          }
+        );
+
         if (response.ok) {
           // Obsługa sukcesu
-          this.setState({ message: "Rezerwacja udana!" });
+          this.setState({
+            message: "Rezerwacja udana!",
+            busyTimes: [...this.state.busyTimes, `${date} ${time}`],
+          });
         } else {
           // Obsługa błędu
-          this.setState({ message: "Wystąpił błąd podczas rezerwacji. Spróbuj ponownie." });
+          const errorData = await response.json(); // Odczytaj dane błędu z odpowiedzi
+          this.setState({
+            message: `Wystąpił błąd: ${errorData.error}. Spróbuj ponownie.`,
+          });
         }
       } catch (error) {
         console.error("Błąd sieci:", error);
+        this.setState({
+          message: "Wystąpił błąd podczas rezerwacji. Spróbuj ponownie.",
+        });
       }
     } else {
-      alert("Soreczka, wybrany termin jest już zajęty. Upoluj sobie inny termin.");
+      alert("Sorki, wybrany termin jest już zajęty. Wybierz inny termin.");
     }
+  };
+
+  calculateEndTime = (startTime, duration) => {
+    const [startHour, startMinute] = startTime.split(":").map(Number);
+    const endMinute = startMinute + duration;
+
+    let endHour = startHour + Math.floor(endMinute / 60);
+    endHour = endHour % 24; 
+    const formattedEndHour = endHour.toString().padStart(2, "0");
+    const formattedEndMinute = (endMinute % 60).toString().padStart(2, "0");
+
+    return `${formattedEndHour}:${formattedEndMinute}`;
   };
 
   render() {
     return (
       <Main>
         <Wrapper>
-        {this.state.message && <div className="message">{this.state.message}</div>}
+          {this.state.message && (
+            <div className="message">{this.state.message}</div>
+          )}
           <Title>Zarezerwuj termin fryzjerski</Title>
           <form onSubmit={this.handleSubmit}>
             <label htmlFor="name">Imię i nazwisko:</label>
@@ -115,7 +198,7 @@ class HaircutReservationForm extends Component {
               <option value="Stylizacja">Stylizacja</option>
               <option value="Inna">Inna (napisz w uwagach)</option>
             </select>
-            
+
             <label htmlFor="selectedOderService">Wybierz inną usługę:</label>
             <select
               id="selectedOderService"
