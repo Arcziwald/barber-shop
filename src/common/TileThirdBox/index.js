55f8a4fbe367a721403d57323a24a05c31bfd3bb
Artur Mochnia <amochnia@gmail.com>
2023-11-21 13:28:03 +0000

    deleted image

--- src/common/TileThirdBox/index.js
+++ src/common/TileThirdBox/index.js
@@ -2,7 +2,6 @@ import { Wrapper, StyledImgGallery } from "./styled";
 import { useImageSlider } from "../TilesList/useImageSlider";
 import standingsolar from "./standingsolar.jpeg";
 import lyingsolar from "./lyingsolar.jpeg";
-import solar from "./solar.jpg";
 
 export const TileThirdBox = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
